b7b27d88eb9b90dc1c90b9bc4e9c2b8f148eb5a9
Chavez <nailsbrokeandfell@gmail.com>
2013-05-10 15:01:16 -0700

    [5] q20

---
+++ 5-beyond-basics/q20.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+  "fmt"
+)
+
+type Person struct {
+  name string
+  age int
+}
+
+type T struct {
+  a int
+}
+
+func Set(t *T) {
+  fmt.Println(t)
+}
+
+func Set2(t T) {
+  fmt.Println(&t)
+}
+
+func main() {
+  var p1 Person
+  p2 := new(Person)
+  fmt.Println(p1)
+  fmt.Println(p2)
+
+  t := T {a: 5}
+  Set(&t)
+  Set2(t)
+}
