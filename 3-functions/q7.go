4043d50524b0d712d5b3945e36303c24986809aa
Chavez <nailsbrokeandfell@gmail.com>
2013-05-13 15:09:58 -0700

    Formatting.

--- 3-functions/q7.go
+++ 3-functions/q7.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-  "fmt"
-  "sort"
+	"fmt"
+	"sort"
 )
 
 type IntSlice []int
 
 // Sort interface functions
 
-func (x IntSlice) Len() int{
-  return len(x)
+func (x IntSlice) Len() int {
+	return len(x)
 }
 
 func (x IntSlice) Less(i, j int) bool {
-  return x[i] < x[j]
+	return x[i] < x[j]
 }
 
 func (x IntSlice) Swap(i, j int) {
-  x[i], x[j] = x[j], x[i]
+	x[i], x[j] = x[j], x[i]
 }
 
 func orderedParams(args ...int) {
-  sort.Sort(IntSlice(args))
-  fmt.Println(args)
+	sort.Sort(IntSlice(args))
+	fmt.Println(args)
 }
 
 func main() {
-  orderedParams(8,3)
-  orderedParams(3,8)
+	orderedParams(8, 3)
+	orderedParams(3, 8)
 }
